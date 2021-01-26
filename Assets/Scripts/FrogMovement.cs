57237497ab650c1ab1a4b39d0a425ca27e270f0a
iknowamouse <noykuozsoz@gmail.com>
2021-01-26 07:53:02 +0300

    enemy movement by powerup

---
+++ Assets/Scripts/FrogMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMovement : MonoBehaviour
+{
+
+   
+    public Animator animatorFrog;
+    public float rightDistance = 10;
+    public float rightMoveSpeed = 0.17f;
+    public bool returnWhenInitial;
+
+    private Vector3 InitialPosition;
+    private float currerntMagnitude;
+    public bool isFrogMove = false;
+    //private bool isFacingRight;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        InitialPosition = transform.position;
+        isFrogMove = false;
+    }
+
+
+    private void Update()
+    {
+      
+            if(isFrogMove == true)
+        {
+                
+                transform.localPosition += (transform.right) * rightMoveSpeed;
+                currerntMagnitude = (transform.localPosition - InitialPosition).magnitude;
+           
+                if (currerntMagnitude > rightDistance)
+                {
+                
+                    rightMoveSpeed *= (-1);
+                
+            }
+
+                if (returnWhenInitial)
+                {
+                    if (transform.localPosition.x < InitialPosition.x)
+                    {
+                        rightMoveSpeed *= (-1);
+                    }
+                }
+                    
+
+        }
+            
+        
+    }
+            // transform.rotation = Quaternion.Euler(0, 180, 0);
+          
+    
+   
+
+}
+
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public Animator frogAnimator;
+    public GameObject frogMove;
+    
+    private void OnTriggerEnter2D(Collider2D power)
+    {
+        
+        if (power.gameObject.CompareTag("Player"))
+        {
+           
+            GameObject.Find("Frog1_Enemy").GetComponent<FrogMovement>().isFrogMove = true; 
+            frogAnimator.SetBool("IfStarCollected", true);
+
+            Destroy(gameObject);
+        }
+    }
+}
